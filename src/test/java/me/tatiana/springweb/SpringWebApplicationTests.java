3dc6d44fd29bdfb8608b9ced9d7647e2d3046b63
tatiana_alekseeva <tatianaalekseeva1313@gmail.com>
2023-01-04 00:27:33 +0300

    proba

---
+++ src/test/java/me/tatiana/springweb/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tatiana.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
