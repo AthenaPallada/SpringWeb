0fba69a5db1bec95af318c98b99cdaeac243c184
tatiana_alekseeva <tatianaalekseeva1313@gmail.com>
2023-01-12 15:19:36 +0300

    homework

--- src/main/java/me/tatiana/springweb/ipml/RecipeServiceImpl.java
+++ src/main/java/me/tatiana/springweb/ipml/RecipeServiceImpl.java
@@ -4,8 +4,7 @@ import me.tatiana.springweb.model.Recipe;
 import me.tatiana.springweb.services.RecipeService;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
@@ -13,12 +12,52 @@ public class RecipeServiceImpl implements RecipeService {
     private static long recipeId = 1;
 
     @Override
-    public void addRecipe(Recipe recipe) {
+    public long addRecipe(Recipe recipe) {
         recipes.put(recipeId++, recipe);
+        return recipeId++;
     }
-
     @Override
     public Recipe getRecipe(long key) {
         return recipes.get(key);
+    }
+
+    @Override
+    public Recipe editRecipe(Recipe recipe, long id) {
+        if (recipes.containsKey(id)) {
+            recipes.put(id, recipe);
+            return recipe;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean removeRecipe(long id) {
+        Recipe recipe = recipes.remove(id);
+        return recipe != null;
+    }
+
+    @Override
+    public Map<Long, Recipe> getAllRecipes() {
+        return recipes;
+    }
+
+    @Override
+    public Map<Long, Recipe> getRecipesByIngredientId(long id1, long id2) {
+        Map<Long, Recipe> recipesContainsIngredient = new TreeMap<>();
+        Iterator<Map.Entry<Long, Recipe>> iter = recipes.entrySet().iterator();
+        if (id2 == 0) {
+            for (Map.Entry<Long, Recipe> one : recipes.entrySet()) {
+                if (one.getValue().getIngredients().containsKey(id1)) {
+                    recipesContainsIngredient.put(one.getKey(), one.getValue());
+                }
+            }
+        } else {
+            for (Map.Entry<Long, Recipe> one : recipes.entrySet()) {
+                if (one.getValue().getIngredients().containsKey(id1) && one.getValue().getIngredients().containsKey(id2)) {
+                    recipesContainsIngredient.put(one.getKey(), one.getValue());
+                }
+            }
+        }
+        return recipesContainsIngredient;
     }
 }
--- src/main/java/me/tatiana/springweb/services/RecipeService.java
+++ src/main/java/me/tatiana/springweb/services/RecipeService.java
@@ -2,9 +2,18 @@ package me.tatiana.springweb.services;
 
 import me.tatiana.springweb.model.Recipe;
 
+import java.util.Map;
+
 public interface  RecipeService {
-    void addRecipe(Recipe recipe);
+    long addRecipe(Recipe recipe);
 
     Recipe getRecipe(long key);
 
+    Recipe editRecipe(Recipe recipe, long id);
+
+    boolean removeRecipe(long id);
+
+    Map<Long, Recipe> getAllRecipes();
+
+    Map<Long, Recipe> getRecipesByIngredientId(long id1, long id2);
 }
